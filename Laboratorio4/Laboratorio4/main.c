e7fae4edbdf9851f69879a74ca755b3d4b65cd0d
rebecafloress <rebecaafloresss@gmail.com>
2026-04-13 07:26:56 -0600

    Post Laboratorio 4

--- Laboratorio4/Laboratorio4/main.c
+++ Laboratorio4/Laboratorio4/main.c
@@ -6,22 +6,17 @@
 // Creado 06/04/2026
 // Descripción: Contador binario 8 bits, con botones de incremento y decremento. También lee el valor de un potenciometro y despliega el valor del ADC
 
-
 // Libraries
 #define F_CPU 16000000
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-
-// Constants & Global Variables
-#define DEBOUNCE_DELAY 50    // ms para antirebote
-
+#define DEBOUNCE_DELAY 50
 volatile uint8_t counter = 0;
-volatile uint8_t btn_up_prev   = 1;   // Estado anterior PB+ (PC2)
-volatile uint8_t btn_down_prev = 1;   // Estado anterior PB- (PC3)
+volatile uint8_t btn_up_prev   = 1;
+volatile uint8_t btn_down_prev = 1;
 
-// Tabla HEX
 const uint8_t seg_table_anodo[16] = {
 	0xC0, // 0
 	0xF9, // 1
@@ -48,45 +43,45 @@ void check_buttons(void);
 void adc_init(void);
 uint16_t adc_read(void);
 void display_hex(uint8_t value);
+void check_alarm(uint8_t adc_val, uint8_t cnt_val);
 
 // Main Function
 int main(void)
 {
-    setup();
+	setup();
 	adc_init();
-    while (1)
-    {
-        check_buttons();
-        uint16_t adc_val = adc_read();
+	while (1)
+	{
+		check_buttons();
+		uint16_t adc_val = adc_read();
 		uint8_t hex_val = adc_val >> 2;
-		
+
 		display_hex(hex_val);
-    }
+		check_alarm(hex_val, counter);
+	}
 }
 
 // NON-Interrupt Subroutines
 void setup(void)
 {
-    cli();
-	
-    DDRB  |= 0x3F;
-    PORTB &= ~0x3F;
+	cli();
 
-    DDRC  |= (1 << PC1);
-    DDRC  &= ~((1 << PC2) | (1 << PC3)); 
-    PORTC |= (1 << PC2) | (1 << PC3);
+	DDRB  |= 0x3F;
+	PORTB &= ~0x3F;
 
-    DDRD = 0x7F;
+	DDRC  |= (1 << PC1);                          
+	DDRC  &= ~((1 << PC2) | (1 << PC3));          
+	PORTC |= (1 << PC2) | (1 << PC3);            
 
-    DDRC |= (1 << PC4) | (1 << PC5);
+	DDRD = 0xFF;
+	DDRC |= (1 << PC4) | (1 << PC5);          
 
-    sei();
+	sei();
 }
 
 void adc_init(void)
 {
-	ADMUX = (1 << REFS0) | (1 << MUX2) | (1 << MUX1);
-
+	ADMUX  = (1 << REFS0) | (1 << MUX2) | (1 << MUX1);
 	ADCSRA = (1 << ADEN)
 	| (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 }
@@ -100,65 +95,65 @@ uint16_t adc_read(void)
 
 void display_hex(uint8_t value)
 {
-    uint8_t high = (value >> 4) & 0x0F;
-    uint8_t low  = value & 0x0F;
+	uint8_t high = (value >> 4) & 0x0F;
+	uint8_t low  =  value       & 0x0F;
 
-    PORTC |= (1 << PC4) | (1 << PC5); // apagar ambos
+	PORTC |=  (1 << PC4) | (1 << PC5);   
+	PORTD  = seg_table_anodo[high];
+	PORTC &= ~(1 << PC4);                
+	_delay_ms(1);
 
-    PORTD = seg_table_anodo[high];    // cargar segmentos
-
-    PORTC &= ~(1 << PC4); // encender izquierdo
-    _delay_ms(1);
-
-    PORTC |= (1 << PC4) | (1 << PC5); // apagar ambos
-
-    PORTD = seg_table_anodo[low];
-
-    PORTC &= ~(1 << PC5); // encender derecho
-    _delay_ms(1);
+	PORTC |=  (1 << PC4) | (1 << PC5);   
+	PORTD  = seg_table_anodo[low];
+	PORTC &= ~(1 << PC5);               
+	_delay_ms(1);
 }
 
 void update_ports(void)
 {
-    PORTB = (PORTB & 0xC0) | (counter & 0x3F);
-
-    PORTC = (PORTC & 0xFC) | ((counter >> 6) & 0x03);
+	PORTB = (PORTB & 0xC0) | (counter & 0x3F);
+	PORTC = (PORTC & 0xFC) | ((counter >> 6) & 0x03);
 }
 
+void check_alarm(uint8_t adc_val, uint8_t cnt_val)
+{
+	if (adc_val > cnt_val)
+	PORTD |=  (1 << PD7);   // Encender LED alarma (punto del display)
+	else
+	PORTD &= ~(1 << PD7);   // Apagar  LED alarma
+}
 // Antirrebote y lectura de botones
 void check_buttons(void)
 {
-    uint8_t btn_up   = (PINC >> PC2) & 0x01;
-    uint8_t btn_down = (PINC >> PC3) & 0x01;
+	uint8_t btn_up   = (PINC >> PC2) & 0x01;
+	uint8_t btn_down = (PINC >> PC3) & 0x01;
 
-    // Botón Incremento (PC2)
-    if (btn_up == 0 && btn_up_prev == 1)
-    {
-        _delay_ms(DEBOUNCE_DELAY);
-        btn_up = (PINC >> PC2) & 0x01;
+	// Botón Incremento (PC2)
+	if (btn_up == 0 && btn_up_prev == 1)
+	{
+		_delay_ms(DEBOUNCE_DELAY);
+		btn_up = (PINC >> PC2) & 0x01;
+		if (btn_up == 0)
+		{
+			counter++;
+			update_ports();
+		}
+	}
 
-        if (btn_up == 0)
-        {
-            counter++;
-            update_ports();
-        }
-    }
+	// Botón Decremento (PC3)
+	if (btn_down == 0 && btn_down_prev == 1)
+	{
+		_delay_ms(DEBOUNCE_DELAY);
+		btn_down = (PINC >> PC3) & 0x01;
+		if (btn_down == 0)
+		{
+			counter--;
+			update_ports();
+		}
+	}
 
-    // Botón Decremento (PC3)
-    if (btn_down == 0 && btn_down_prev == 1)
-    {
-        _delay_ms(DEBOUNCE_DELAY);
-        btn_down = (PINC >> PC3) & 0x01;
-
-        if (btn_down == 0)
-        {
-            counter--;
-            update_ports();
-        }
-    }
-
-    btn_up_prev   = btn_up;
-    btn_down_prev = btn_down;
+	btn_up_prev   = btn_up;
+	btn_down_prev = btn_down;
 }
 
 // Interrupt Routines
